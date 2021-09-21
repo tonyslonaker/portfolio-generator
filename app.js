3e1705296c026f19599b2ef01656da41221d7f37
Tony Slonaker <tony.slonaker@gmail.com>
2021-09-20 20:17:54 -0600

    Update FS module

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//const fs = require('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
-//const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
   return inquirer.prompt([
